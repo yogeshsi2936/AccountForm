f05942071e4e21c385a712b3ffba1124974850ab
yogeshsi2936 <yogesh.mohare@sportzinteractive.net>
2023-07-21 20:48:23 +0530

    code clean

--- src/components/AccountForm2.vue
+++ src/components/AccountForm2.vue
@@ -5,8 +5,8 @@
     <div class="">
       <label>First Name</label>
       <p>
-        <input name="firstNameValidator" v-model="firstname" v-validate="'required|firstNameValidator'" type="text" placeholder="First Name">
-        <span class="errorMsg" v-show="errors.has('firstNameValidator')">{{ errors.first('firstNameValidator') }}</span>
+        <input name="alpha_spaces_field" v-model="firstname" v-validate="'required|alpha_spaces|alpha_num'" type="text" placeholder="First Name">
+        <span class="errorMsg" v-show="errors.has('alpha_spaces_field')">{{ errors.first('alpha_spaces_field') }}</span>
       </p>
     </div>
 
@@ -14,8 +14,8 @@
     <div class="">
       <label>Last Name</label>
       <p>
-        <input name="lastNameValidator" v-model="lastname" v-validate="'required|firstNameValidator'" type="text" placeholder="Last Name">
-        <span class="errorMsg" v-show="errors.has('lastNameValidator')">{{ errors.first('lastNameValidator') }}</span>
+        <input name="alpha_spaces_field" v-model="lastname" v-validate="'required|alpha_spaces|alpha_num'" type="text" placeholder="Last Name">
+        <span class="errorMsg" v-show="errors.has('alpha_spaces_field')">{{ errors.first('alpha_spaces_field') }}</span>
       </p>
     </div>
 
@@ -32,59 +32,57 @@
     <div class="">
       <label>Phone Number</label>
       <p>
-        <input name="phoneValidator"  v-model="phone" v-validate="{required:true ,numeric:true,phoneValidator:country }" type="number" placeholder="Phone">
-        <span class="errorMsg" v-show="errors.has('phoneValidator')">{{ errors.first('phoneValidator') }}</span>
+        <input name="phoneNumberValidator"  v-model="phone" v-validate="{required:true ,numeric:true,phoneNumberValidator:country }" type="number" placeholder="Phone">
+        <span class="errorMsg" v-show="errors.has('phoneNumberValidator')">{{ errors.first('phoneNumberValidator') }}</span>
       </p>
     </div>
     <!-- Gender -->
     <div>
       <p><label> Gender </label></p>
       <label class="radio is-inline" v-for="gendr in genderList" :key="gendr">
-        <input type="radio" v-model="gender" :value="gendr" class="radioBtn" v-validate="'required|genderValidator'" name="genderValidator">
+        <input type="radio" v-model="gender" :value="gendr" class="radioBtn" v-validate="'required'" name="required">
         <span class="radioBtn">{{ gendr }}</span>
       </label>
-      <span class="errorMsg" v-show="errors.has('genderValidator')">{{ errors.first('genderValidator') }}</span>
+      <span class="errorMsg" v-show="errors.has('required')">{{ errors.first('required') }}</span>
 
     </div>
 
     <!-- Country -->
     <div>
-      <p><label for="selectedOption">Select Country</label></p>
-      <select id="selectedOption" v-model="country" v-validate="'required|countrySelectValidator'" name="countrySelectValidator">
+      <p><label>Select Country</label></p>
+      <select  v-model="country" v-validate="'required'" name="name">
         <option value="" disabled>Please select country</option>
         <option v-for="option in countryList" :key="option.value" :value="option.value">{{ option.label }}</option>
       </select>
-      <span class="errorMsg" v-show="errors.has('countrySelectValidator')">{{ errors.first('countrySelectValidator') }}</span>
+      <span class="errorMsg" v-show="errors.has('name')">{{ errors.first('name') }}</span>
     </div>
 
     <!-- state -->
     <div>
       <div v-if="country === 'india'">
-        <p><label for="selectedOption">Select state </label></p>
-        <select id="selectedOption" v-model="state" v-validate="{required:true , stateValidator:country }" name="stateValidator" >
+        <p><label>Select state </label></p> 
+        <select  v-model="state" v-validate="{required:true }" name="required" >
           <option value="" disabled>Please select state</option>
-          <option v-for="state in stateList" :key="state" :value="state">{{ state }}</option>
+          <option v-for="state in stateList" :key="state.value" :value="state.value">{{ state.value }}</option>
         </select>
-        <span class="errorMsg" v-show="errors.has('stateValidator')">{{ errors.first('stateValidator') }}</span>
+        <span class="errorMsg" v-show="errors.has('required')">{{ errors.first('required') }}</span>
       </div>
 
       <div v-else>
         <label>State</label>
         <div>
-          <input v-validate="'required|stateValidator'" type="text" name="stateValidator" placeholder="state name">
-          <span class="errorMsg" v-show="errors.has('stateValidator')">{{ errors.first('stateValidator') }}</span>
+          <input v-validate="'required'" type="text" name="required" placeholder="state name">
+          <span class="errorMsg" v-show="errors.has('required')">{{ errors.first('required') }}</span>
         </div>
       </div>
 
     <!-- Birthdate -->
       <div>
-        <p> <label for="dateInput">Birthdate</label></p>
-        <input v-validate="{required:true , birthDate:[ageLimit,selectedDate], }" type="date" id="dateInput" v-model="selectedDate" name="birthDate">
-        <span class="errorMsg" v-show="errors.has('birthDate')">{{ errors.first('birthDate') }}</span>
+        <p> <label for="dateInput">Date of Birth</label></p>
+        <input v-validate="{required:true , DateValidator:[ageLimit,birthDate], }" type="date" id="dateInput" v-model="birthDate" name="DateValidator">
+        <span class="errorMsg" v-show="errors.has('DateValidator')">{{ errors.first('DateValidator') }}</span>
       </div>
     </div>
-
-
     <div class="">
       <p>
         <button class="button is-primary" type="submit">Submit</button>
@@ -102,12 +100,10 @@ export default {
     firstname: '',
     lastname: '',
     phone: '',
-    url: '',
     gender: '',
     country: '',
     state: '',
-    birthdate: '',
-    selectedDate: '',
+    birthDate: '',
     ageLimit :18,
     genderList: ['male', 'female'],
     countryList: [
@@ -119,9 +115,18 @@ export default {
       { value: 'russia', label: 'Russia' },
       { value: 'sing', label: 'Singapore' },
     ],
-    stateList: ['Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Karnataka'
-      , 'Kerala', 'Maharashtra', 'Meghalaya', 'Rajasthan', 'West Bengal'],
-
+    stateList: [
+      { value: 'andhrapradhesh', label: 'Andhra Pradesh' },
+      { value: 'arunachalpradhesh', label: 'Arunachal Pradesh' },
+      { value: 'assam', label: 'Assam' },
+      { value: 'chhattisgarh', label: 'Chhattisgarh' },
+      { value: 'meghalaya', label: 'Meghalaya' },
+      { value: 'maharashtra', label: 'Maharashtra' },
+      { value: 'kerala', label: 'Kerala' },
+      { value: 'rajsthan', label: 'Rajasthan' },
+      { value: 'bihar', label: 'Bihar' },
+      { value: 'westbengal', label: 'West Bengal' },
+    ],
   }),
   methods: {
     validateBeforeSubmit() {
@@ -132,7 +137,7 @@ export default {
           return;
         }
 
-        alert('Correct them errors!');
+        alert('Please fill all required fields');
       });
     }
   }
--- src/main.js
+++ src/main.js
@@ -2,38 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import VeeValidate from 'vee-validate';
 import { Validator } from 'vee-validate';
+import './assets/main.css'
 
-Validator.extend('firstNameValidator', {
-  getMessage: (field) =>{
-      console.log(field);
-     `${field} can only contain alphanumeric characters and spaces.`
-  },
-  validate: (value) => /^[a-zA-Z0-9\s]+$/.test(value),
-});
-Validator.extend('birthDate', {
+Validator.extend('DateValidator', {
   getMessage: (field,args) => {
     const ageLimit = args[0] ;
-    const birthDate = new Date(args[1]);
+    const selectedDate = new Date(args[1]);
     const currentDate = new Date();
-    const timeDiffInMilliseconds = currentDate - birthDate;
-    const oneYearInMilliseconds = 1000 * 60 * 60 * 24 * 365.25; 
-    const ageDifferenceInYears  = Math.floor(timeDiffInMilliseconds / oneYearInMilliseconds);
-     if (ageDifferenceInYears < 0 ) {
-      return 'please select proper age'
-    }else if (ageDifferenceInYears < ageLimit) {
-      return `Your are not eligible age limit is ${ageLimit}`
+    const currentAge =  getCurrentAge(currentDate,selectedDate)
+     if (currentAge < 0 ) {
+      return 'Please select proper date'
+    }else if (currentAge < ageLimit) {
+      return `The age must be greater than ${ageLimit}`
     }
   },
   validate: (value,args) => {
     const ageLimit = args[0] ;
     const selectedDate = new Date(value);
     const currentDate = new Date();
-    const timeDiffInMilliseconds = currentDate - selectedDate;
-    const oneYearInMilliseconds = 1000 * 60 * 60 * 24 * 365.25; 
-    const ageDifferenceInYears  = Math.floor(timeDiffInMilliseconds / oneYearInMilliseconds);
-    if (ageDifferenceInYears <= 0 ) {
+    const currentAge =  getCurrentAge(currentDate,selectedDate)
+    if (currentAge <= 0 ) {
       return false
-    }else if (ageDifferenceInYears < ageLimit) {
+    }else if (currentAge < ageLimit) {
       return false
     }else {
       return true
@@ -41,34 +31,10 @@ Validator.extend('birthDate', {
   },
 });
 
-Validator.extend('stateValidator', {
-  getMessage: () => "State must have atleast 5 min character.",
-  validate: (value, [otherValue]) => {
-    if (otherValue === 'india') {
-      return true
-    }
-    const isValidState = value.trim().length >= 5 ?  true : false
-    return isValidState
-  },
-});
-
-Validator.extend('phoneValidator', {
-  getMessage: (field,args) => {
-    if (args[0].trim().length === 0) {
-      return  "Please select one country first." 
-    }
-    else if (args[0] === 'india') {
-      return 'Please entre mobile no with 10 digit'
-    }else{
-      return 'Please entre mobile min 5 digit'
-    }
-  },
-
+Validator.extend('phoneNumberValidator', {
+  getMessage: (field) => `please enter valid phone number `,
   validate: (value,[otherValue]) => {
-    if (otherValue === '') {
-      return false
-    }
-    else if (otherValue === 'india') {
+     if (otherValue === 'india') {
       const val = value.trim().length === 10  ? true : false ;
       return val
     }else {
@@ -77,31 +43,14 @@ Validator.extend('phoneValidator', {
     }
   },
 });
+function getCurrentAge(currentDate, selectedDate) {
+  const timeDiffInMilliseconds = currentDate - selectedDate;
+  const oneYearInMilliseconds = 1000 * 60 * 60 * 24 * 365.25; 
+  const ageDifferenceInYears  = Math.floor(timeDiffInMilliseconds / oneYearInMilliseconds);
+  return ageDifferenceInYears
+}
 
-Validator.extend('genderValidator', {
-  getMessage: () => "Gender Must be selected.",
-  validate: (value) => {
-    if (value.trim().length > 0) {
-      return true
-    }else {
-      return false
-    }
-  },
-});
-
-Validator.extend('countrySelectValidator', {
-  getMessage: () => "Country Must be selected.",
-  validate: (value) => {
-    if (value.trim().length > 0) {
-      return true
-    }else {
-      return false
-    }
-  },
-});
 Vue.use(VeeValidate);
-
-import './assets/main.css'
 
 new Vue({
   render: (h) => h(App)
